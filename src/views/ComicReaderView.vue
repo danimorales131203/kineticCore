fb0413557d21c64db27aad85456f2df0a5a5258a
Daniel Morales <adanielmorales0@gmail.com>
2026-05-31 17:32:10 -0700

    Estilos dinamicos de lectura

--- src/views/ComicReaderView.vue
+++ src/views/ComicReaderView.vue
@@ -23,20 +23,36 @@ const emit = defineEmits([
 
 const currentPage = ref(0)
 const readingMode = ref('static')
+const animationType = ref('pageFlip')
+const animationKey = ref(0)
 
 const pages = computed(() => props.comic.pages || [])
 const currentPageImage = computed(() => pages.value[currentPage.value])
 
+function triggerAnimation() {
+  animationKey.value++
+}
+
 function nextPage() {
   if (currentPage.value < pages.value.length - 1) {
     currentPage.value++
+    triggerAnimation()
   }
 }
 
 function previousPage() {
   if (currentPage.value > 0) {
     currentPage.value--
+    triggerAnimation()
   }
+}
+
+function getAnimationClass() {
+  if (readingMode.value !== 'dynamic') {
+    return ''
+  }
+
+  return animationType.value
 }
 </script>
 
@@ -52,24 +68,34 @@ function previousPage() {
 
     <section class="comic-reader-layout">
       <div class="comic-page-display">
+        <button
+          class="page-side-arrow page-side-arrow-left"
+          @click="previousPage"
+        >
+          ‹
+        </button>
+
         <p v-if="pages.length === 0">
           No pages found for this comic.
         </p>
 
         <img
           v-else
-          :key="currentPageImage"
+          :key="`${currentPageImage}-${animationKey}`"
           :src="currentPageImage"
           :alt="`${comic.title} page ${currentPage + 1}`"
-          :class="{ dynamic: readingMode === 'dynamic' }"
+          :class="getAnimationClass()"
         />
+
+        <button
+          class="page-side-arrow page-side-arrow-right"
+          @click="nextPage"
+        >
+          ›
+        </button>
       </div>
 
       <div class="comic-reader-controls">
-        <button class="reader-arrow" @click="previousPage">
-          ←
-        </button>
-
         <button class="reader-close" @click="emit('go-back')">
           Close
         </button>
@@ -80,16 +106,30 @@ function previousPage() {
           <span>Page {{ currentPage + 1 }} / {{ pages.length }}</span>
         </div>
 
-        <button
-          class="reader-mode-toggle"
-          @click="readingMode = readingMode === 'static' ? 'dynamic' : 'static'"
-        >
-          {{ readingMode === 'static' ? 'Dynamic' : 'Static' }}
-        </button>
+        <div class="reader-animation-controls">
+          <button
+            :class="{ active: readingMode === 'static' }"
+            @click="readingMode = 'static'"
+          >
+            Static
+          </button>
 
-        <button class="reader-arrow" @click="nextPage">
-          →
-        </button>
+          <button
+            :class="{ active: readingMode === 'dynamic' }"
+            @click="readingMode = 'dynamic'"
+          >
+            Dynamic
+          </button>
+
+          <select
+            v-model="animationType"
+            :disabled="readingMode !== 'dynamic'"
+          >
+            <option value="pageFlip">Page Flip</option>
+            <option value="speedDash">Speed Dash</option>
+            <option value="webSwing">Web Swing</option>
+          </select>
+        </div>
       </div>
     </section>
   </main>
